7cdbddd2be436f177bf62adffb0d05ca4eed13ef
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-30 23:23:14 -0400

    write first enzyme test, for bs item card class

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "./App";
 
-// it() is Jest's test function, which takes 2 args:
-// 1st arg: name of the test to display,
-// 2nd arg: arrow function of the test to run.
+// the basic Jest test
 it("renders without crashing", () => {
-  // create variable for a <div> named "div"
   const div = document.createElement("div");
-  // use ReactDOM to render the App component into a new <div>
   ReactDOM.render(<App />, div);
-  // and then complete the lifecycle by unmounting the test <div>
   ReactDOM.unmountComponentAtNode(div);
+});
+
+// our first Enzyme DOM test
+// on first run, we need to make sure that it FAILS,
+// since it is testing something that doesn't exist yet.
+it("renders a div with the className set to be an item card", () => {
+  // create a shallow enzyme wrapper for App component
+  const wrapper = shallow(<App />);
+  // then write the expect() function, passing in what you expect
+  expect(
+    // on the (shallow App) wrapper...
+    wrapper
+      // return another wrapper just the divs in App,
+      .find("div")
+      // iterate over all divs, and where (using props()) a div
+      // has prop of className="list-item-card", return all matches
+      // get the length of the returned array of all matches
+      .findWhere(e => e.props().className === "list-item-card").length
+  )
+    // and then we expect() the resulting .length to be: 1.
+    // if the value in toBe() matches our expect() value, test passes.
+    .toBe(1);
 });
