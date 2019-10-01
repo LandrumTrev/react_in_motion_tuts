cf93d5c0529661caa0f2773434236a8cb59e57d9
LandrumTrev <landrum.trevillian@gmail.com>
2019-09-30 23:38:42 -0400

    apparently screwed up node_modules, restart with a new CRA

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      things: 0
-    };
-  }
-
   render() {
     return (
-      <div>
-        {/* blank setup */}
-        {/* blank setup */}
+      <div className="list-item-card">
+        {/* stuff */}
+        Well, hello there world!
       </div>
     );
   }
